aa6654f605d885467e7539c2d846f6e52a8b5723
mikeluan <luancfan@hotmail.com>
2015-02-12 11:38:06 +0800

    Fixing Visualstudio Project Files

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,13 +1,54 @@
 module.exports = function(grunt) {
+    //grunt.initConfig({
+    //    ts: {
+    //        default : {
+    //            src: ["**/*.ts", "!node_modules/**/*.ts"],
+    //            watch: ".",
+    //            target: 'es5',
+    //            outDir: './_Releases',
+    //            options: {
+    //                fast: "watch",
+    //                module: "commonjs",
+    //                sourceMap: false,
+    //                suppressImplicitAnyIndexErrors: true,
+    //                preserveConstEnums: true
+    //            }
+    //        }
+    //    }
+    //});
+
+
+
     grunt.initConfig({
+        watch: {
+            scripts: {
+                files: ['**/*.ts'],
+                tasks: ['ts'],
+                options: {
+                    spawn: false,
+                },
+            },
+        },
         ts: {
             default : {
                 src: ["**/*.ts", "!node_modules/**/*.ts"],
                 target: 'es5',
-                outDir: './_Releases'
+                outDir: './_Releases',
+                options: {
+                    fast: "never",
+                    module: "commonjs",
+                    sourceMap: false,
+                    suppressImplicitAnyIndexErrors: true,
+                    preserveConstEnums: true
+                }
             }
         }
     });
+    grunt.loadNpmTasks('grunt-contrib-watch');
     grunt.loadNpmTasks("grunt-ts");
-    grunt.registerTask("default", ["ts"]);
+    grunt.registerTask("default", ['ts']);
+    grunt.registerTask("w", ['ts', 'watch']);
+
+    //grunt.loadNpmTasks("grunt-ts");
+    //grunt.registerTask("default", ["ts"]);
 };
