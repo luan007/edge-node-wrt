0f7963d6b7194515651e2a76a88dcd84753c7445
Locke <moyerock@gmail.com>
2015-02-21 21:58:11 +0800

    support for variable deployment target.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,56 +1,63 @@
 module.exports = function(grunt) {
 
-    grunt.initConfig({
-        watch: {
-            scripts: {
-                files: ['**/*.ts'],
-                tasks: ['ts'],
-                options: {
-                    spawn: false
+    //target folder
+    var destination = './_Releases/';
+    var sambaFolder = '//192.168.222.132/Release'; //smb://serbver/folder/
+
+    function initGrunt(destination) {
+        grunt.initConfig({
+            watch: {
+                scripts: {
+                    files: ['**/*.ts'],
+                    tasks: ['ts'],
+                    options: {
+                        spawn: false
+                    }
+                },
+                staticFiles: {
+                    files: ['Applications/**/*.*', '!Applications/**/*.ts'],
+                    tasks: ['copy', 'sync'],
+                    options: {
+                        spawn: false
+                    }
                 }
             },
-            staticFiles: {
-                files: ['Applications/**/*.*', '!Applications/**/*.ts'] ,
-                tasks: ['copy', 'sync'],
-                options: {
-                    spawn: false
+            clean: [destination],
+            copy: {
+                main: {
+                    files: [
+                        {expand: true, src: ['Misc/rootDir/*.*'], flatten: true, dest: destination, filter: 'isFile'}
+                    ]
+                }
+            },
+            sync: {
+                main: {
+                    files: [
+                        {src: ['Applications/**/*.*', '!Applications/**/*.ts'], dest: destination},
+                        {src: ['Modules/**/*.*', '!Modules/**/*.ts'], dest: destination},
+                        {src: ['Orbit/**/*.*', '!Orbit/**/*.ts'], dest: destination},
+                        {src: ['System/**/*.*', '!System/**/*.ts'], dest: destination},
+                    ],
+                    verbose: true // Display log messages when copying files
+                }
+            },
+            ts: {
+                default: {
+                    src: ['**/*.ts', '!node_modules/**/*.ts'],
+                    target: 'es5',
+                    outDir: destination,
+                    options: {
+                        fast: "never",
+                        module: "commonjs",
+                        sourceMap: false,
+                        suppressImplicitAnyIndexErrors: true,
+                        preserveConstEnums: true
+                    }
                 }
             }
-        },
-        clean: ['./_Releases'],
-        copy: {
-            main: {
-                files: [
-                    {expand: true, src: ['Misc/rootDir/*.*'],   flatten: true , dest: './_Releases/', filter: 'isFile'}
-                ]
-            }
-        },
-        sync: {
-            main: {
-                files: [
-                    {src: ['Applications/**/*.*', '!Applications/**/*.ts']    , dest: './_Releases'},
-                    {src: ['Modules/**/*.*',      '!Modules/**/*.ts']         , dest: './_Releases'},
-                    {src: ['Orbit/**/*.*',        '!Orbit/**/*.ts']           , dest: './_Releases'},
-                    {src: ['System/**/*.*',       '!System/**/*.ts']          , dest: './_Releases'},
-                ],
-                verbose: true // Display log messages when copying files
-            }
-        },
-        ts: {
-            default : {
-                src: ['**/*.ts', '!node_modules/**/*.ts'],
-                target: 'es5',
-                outDir: './_Releases',
-                options: {
-                    fast: "never",
-                    module: "commonjs",
-                    sourceMap: false,
-                    suppressImplicitAnyIndexErrors: true,
-                    preserveConstEnums: true
-                }
-            }
-        }
-    });
+        });
+    }
+    initGrunt(destination);
     grunt.loadNpmTasks('grunt-contrib-watch');
     grunt.loadNpmTasks('grunt-contrib-copy');
     grunt.loadNpmTasks('grunt-sync');
@@ -61,6 +68,10 @@ module.exports = function(grunt) {
     grunt.registerTask("w", ['clean', 'copy','sync', 'ts', 'watch']);
     grunt.registerTask("build", ['copy', 'sync', 'ts']);
     grunt.registerTask("debug", ['copy', 'sync', 'ts', 'watch']);
+    grunt.registerTask("samba","samba deployment", function(){
+        initGrunt(sambaFolder);
+        grunt.task.run('debug');
+    });
 
     //grunt.loadNpmTasks("grunt-ts");
     //grunt.registerTask("default", ["ts"]);
