e2c9e933703a812e1c8e3d80464e0a953dd3fdbb
rockdragon <moyerock@gmail.com>
2015-05-14 11:40:41 +0800

    Graphd test case: OK.

--- SYS/Device/Graphd/__Test.ts
+++ SYS/Device/Graphd/__Test.ts
@@ -7,7 +7,20 @@ import DB = require('./DB');
 export function Initialize(cb) {
     fatal('============>>> device: Graphd testing...');
 
+    DB.RebuildDeltaV(()=> {
+        var $query = {
+            $and: [
+                {type: 1},
+                {
+                    tag: "vendor"
+                }
+            ]
+        };
+        DB.Find($query, (err, des) => {
+            if (err) error(err);
+            else fatal('============>>> device: Graphd result:', des);
+        });
 
-
-    cb();
+        cb();
+    });
 }
