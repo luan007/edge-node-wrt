5efe3003bd3eb87e85a13e2ababeeef5c7f212a7
Locke <moyerock@gmail.com>
2015-06-08 21:45:08 +0800

    fix a encrypting issue: a holy omission.

--- Orbit/Common/AES.ts
+++ Orbit/Common/AES.ts
@@ -7,14 +7,14 @@ import child_process = require('child_process');
 var algorithm = 'aes-256-cbc';
 var passwdLength = 256;
 
-export function RandomPassword(bytesLength=passwdLength) { 
+export function RandomPassword(bytesLength = passwdLength) {
     return crypto.randomBytes(bytesLength).toString('hex');
 }
 
-export function EncryptFileSteram(filePath:string, password:string): stream.Readable {
-    return child_process.spawn('openssl', ['enc', '-k', password, '-in', filePath]).stdout;
+export function EncryptFileSteram(filePath:string, password:string):stream.Readable {
+    return child_process.spawn('openssl', ['enc', '-e', algorithm, '-k', password, '-in', filePath]).stdout;
 }
 
-export function DecryptFileStream(encryptedFilePath:string, password:string):stream.Readable {
-    return child_process.spawn('openssl', ['enc', '-d', '-k', password, '-in', encryptedFilePath]).stdout;
-}
+//export function DecryptFileStream(encryptedFilePath:string, password:string):stream.Readable {
+//    return child_process.spawn('openssl', ['enc', '-d', '-k', password, '-in', encryptedFilePath]).stdout;
+//}
