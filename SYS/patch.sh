4769c99647519f16e581d2706a7c97083bb90112
Mike Luan <luancfan@hotmail.com>
2015-07-20 17:51:16 +0800

    retract

--- SYS/patch.sh
+++ SYS/patch.sh
@@ -2,7 +2,7 @@
 
 ln -s /dev/mmcblk0p1 /dev/root
 chmod 777 APP/Sandbox/net.sh
-chmod -R 644 /ramdisk/Modules/Static
+#chmod -R 644 /ramdisk/Modules/Static
 if [ -e /node_modules/sqlite3/lib/binding/node-v11-linux-ia32  ]; then
     #rm -rf /node_modules/sqlite3/lib/binding/node-v11-linux-arm
     mv /node_modules/sqlite3/lib/binding/node-v11-linux-ia32 /node_modules/sqlite3/lib/binding/node-v11-linux-arm
