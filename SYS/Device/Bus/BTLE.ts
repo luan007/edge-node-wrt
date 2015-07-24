78ee2f29197c383c07aa274c26663bcf4cf3c988
rockdragon <moyerock@gmail.com>
2015-07-24 21:40:41 +0800

    BTLE bus with Noble.

--- SYS/Device/Bus/BTLE.ts
+++ SYS/Device/Bus/BTLE.ts
@@ -2,56 +2,23 @@ import Bus = require("./Bus");
 import Gatttool = require('../../Common/Native/gatttool');
 var _btleBus = new Bus('BTLE');
 
-var mac_list = {};
-function __exists(MAC) {
-    return has(mac_list, MAC);
-}
-function __add(MAC) {
-    if (!has(mac_list, MAC))
-        mac_list[MAC] = 1;
+function _on_device_appear(mac, device) {
+    _btleBus.DeviceUp(mac, device);
 }
 
-function _on_device_appear(mac, name) {
-    if(!__exists(mac)){
-        _btleBus.DeviceUp(mac, {
-            name: name,
-            BTLE: {}
-        });
-        __add(mac);
-
-        var jobs = [];
-        jobs.push((cb)=> {
-            Gatttool.Primary(CONF.DEV.BLUETOOTH.DEV_HCI, mac, (err, primary) => {
-                cb();
-                if (err) return error(err);
-                _btleBus.DeviceUp(mac, {
-                    BTLE: {
-                        primary: primary
-                    }
-                });
-            });
-        });
-        jobs.push((cb)=> {
-            Gatttool.Characteristics(CONF.DEV.BLUETOOTH.DEV_HCI, mac, (err, characteristics) => {
-                cb();
-                if (err) return error(err);
-                _btleBus.DeviceUp(mac, {
-                    BTLE: {
-                        characteristics: characteristics
-                    }
-                });
-            });
-        });
-        async.series(jobs, ()=> {
-        });
-    }
+function _on_device_disappear(mac){
+    _btleBus.DeviceDrop(mac);
 }
 
 export function Subscribe(cb) {
     var sub = StatMgr.Sub(SECTION.BTLE);
 
-    sub.devices.on('set', (mac, oldValue, name)=> {//Found
-        _on_device_appear(mac, name);
+    sub.devices.on('set', (mac, oldValue, device)=> {//Found
+        _on_device_appear(mac, device);
+    });
+
+    sub.devices.on('del', (mac, oldValue)=>{
+        _on_device_disappear(mac);
     });
 
     cb();
