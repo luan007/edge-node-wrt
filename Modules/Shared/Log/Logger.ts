368002c8d7dcf3a1ac1cb6c23e220f992e79bf5a
rockdragon <moyerock@gmail.com>
2015-07-29 17:08:45 +0800

    test new Logger.

--- Modules/Shared/Log/Logger.ts
+++ Modules/Shared/Log/Logger.ts
@@ -1,106 +1,110 @@
 import FileLogger = require('./FileLogger');
 import hook = require('./leHook');
-var log4js:any = require('log4js');
 
-var ns:any = {};
-var delimiter = ":";
+(() => {
+    var ns:any = {};
+    var delimiter = ":";
 
+    hook.Passthrough('stderr', process.stderr);
 
-hook.Passthrough('stderr', process.stderr);
-
-log4js.configure({
-    appenders: [
-        {
-            type: 'console',
-            layout: {
-                type: 'pattern',
-                pattern: "%[%d{ABSOLUTE} %c%] %m"
+    var log4js:any = require('log4js');
+    log4js.configure({
+        appenders: [
+            {
+                type: 'console',
+                layout: {
+                    type: 'pattern',
+                    pattern: "%[%d{ABSOLUTE} %c%] %m"
+                }
             }
-        }
-    ]
-});
+        ]
+    });
 
-export function GetLogger(moduleName:string, sw?:boolean){
-    moduleName = moduleName.toLowerCase();
-    if(sw === undefined) sw = true;
-    Turn(moduleName, sw);
+    function GetLogger(moduleName:string, sw?:boolean) {
+        moduleName = moduleName.toLowerCase();
+        if (sw === undefined) sw = true;
+        Turn(moduleName, sw);
 
-    var logger:any = {};
+        var logger:any = {};
 
-    logger.trace = (...args) => {
-        if(__isOn(moduleName)) {
-            var logger = log4js.getLogger(moduleName);
-            logger.trace.apply(logger, args);
-            hook.DoLog('trace', args.toString());
-        }
-    };
+        logger.trace = (...args) => {
+            if (__isOn(moduleName)) {
+                var logger = log4js.getLogger(moduleName);
+                logger.trace.apply(logger, args);
+                hook.DoLog('trace', args.toString());
+            }
+        };
 
-    logger.warn = (...args) => {
-        if(__isOn(moduleName)) {
-            var logger = log4js.getLogger(moduleName);
-            logger.warn.apply(logger, args);
-            hook.DoLog('warn', args.toString());
-        }
-    };
+        logger.warn = (...args) => {
+            if (__isOn(moduleName)) {
+                var logger = log4js.getLogger(moduleName);
+                logger.warn.apply(logger, args);
+                hook.DoLog('warn', args.toString());
+            }
+        };
 
-    logger.fatal = (...args) => {
-        if(__isOn(moduleName)) {
-            var logger = log4js.getLogger(moduleName);
-            logger.fatal.apply(logger, args);
-            hook.DoLog('fatal', args.toString());
+        logger.fatal = (...args) => {
+            if (__isOn(moduleName)) {
+                var logger = log4js.getLogger(moduleName);
+                logger.fatal.apply(logger, args);
+                hook.DoLog('fatal', args.toString());
 
-            FileLogger.fatal(args);
-        }
-    };
+                FileLogger.fatal(args);
+            }
+        };
 
-    logger.debug = (...args) => {
-        if(__isOn(moduleName)) {
-            var logger = log4js.getLogger(moduleName);
-            hook.DoLog('debug', args.toString());
-            logger.debug.apply(logger, args);
-        }
-    };
+        logger.debug = (...args) => {
+            if (__isOn(moduleName)) {
+                var logger = log4js.getLogger(moduleName);
+                hook.DoLog('debug', args.toString());
+                logger.debug.apply(logger, args);
+            }
+        };
 
-    logger.error = (...args) => {
-        if(__isOn(moduleName)) {
-            var logger = log4js.getLogger(moduleName);
-            logger.error.apply(logger, args);
+        logger.error = (...args) => {
+            if (__isOn(moduleName)) {
+                var logger = log4js.getLogger(moduleName);
+                logger.error.apply(logger, args);
 
-            hook.DoLog('error', args.toString());
-            FileLogger.error(args);
-        }
-    };
+                hook.DoLog('error', args.toString());
+                FileLogger.error(args);
+            }
+        };
 
-    logger.info = (...args) => {
-        if(__isOn(moduleName)) {
-            var logger = log4js.getLogger(moduleName);
-            logger.info.apply(logger, args);
-            hook.DoLog('info', args.toString());
-        }
-    };
+        logger.info = (...args) => {
+            if (__isOn(moduleName)) {
+                var logger = log4js.getLogger(moduleName);
+                logger.info.apply(logger, args);
+                hook.DoLog('info', args.toString());
+            }
+        };
 
-    return logger;
-}
-
-function __isOn(moduleName){
-    var nodes = moduleName.toLowerCase().split(delimiter);
-    var cur = ns;
-    for(var i = 0, len = nodes.length; i< len; i++) {
-        var n = nodes[i];
-        if(!cur[n]) return false;
-        cur = cur[n];
-        if(cur.hasOwnProperty("sw") && cur.sw === false) return false;
+        return logger;
     }
-    return true;
-}
 
-export function Turn(moduleName:string, sw?:boolean){
-    var nodes = moduleName.toLowerCase().split(delimiter);
-    var cur = ns;
-    for(var i = 0, len = nodes.length; i< len; i++) {
-        var n = nodes[i];
-        if(!cur[n]) cur[n] = {};
-        cur = cur[n];
-        if(i === len - 1) cur.sw = !!sw;
+    function __isOn(moduleName) {
+        var nodes = moduleName.toLowerCase().split(delimiter);
+        var cur = ns;
+        for (var i = 0, len = nodes.length; i < len; i++) {
+            var n = nodes[i];
+            if (!cur[n]) return false;
+            cur = cur[n];
+            if (cur.hasOwnProperty("sw") && cur.sw === false) return false;
+        }
+        return true;
     }
-}
+
+    function Turn(moduleName:string, sw?:boolean) {
+        var nodes = moduleName.toLowerCase().split(delimiter);
+        var cur = ns;
+        for (var i = 0, len = nodes.length; i < len; i++) {
+            var n = nodes[i];
+            if (!cur[n]) cur[n] = {};
+            cur = cur[n];
+            if (i === len - 1) cur.sw = !!sw;
+        }
+    }
+
+    global.GetLogger = GetLogger;
+    global.Turn = Turn;
+})();
