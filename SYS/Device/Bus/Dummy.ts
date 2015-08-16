6cfd80f1151ea91656d9e5ec5562c0745e7a048c
Mike Luan <luancfan@hotmail.com>
2015-08-16 20:45:05 +0800

    log

--- SYS/Device/Bus/Dummy.ts
+++ SYS/Device/Bus/Dummy.ts
@@ -6,11 +6,11 @@ var dummy = new Bus('DUMMY');
 
 SYS_ON(SYS_EVENT_TYPE.LOADED, ()=>{
     setTimeout(function(){
-        console.log("RUNNING TEST, GENERATING NEW DUMMY DEVICE");
+        trace("RUNNING TEST, GENERATING NEW DUMMY DEVICE");
         dummy.DeviceUp("00:00:00:00:00:00", { /*Nothing*/ });
     }, 5000);
     setTimeout(function(){
-        console.log("RUNNING TEST #2, GENERATING DATA");
+        trace("RUNNING TEST #2, GENERATING DATA");
         dummy.DeviceUp("00:00:00:00:00:00", { 
             SettleTime: Date.now()
         });
