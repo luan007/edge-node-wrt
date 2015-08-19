b16c491c10ef0e4e898356f288b6ad06affdc854
rockdragon <moyerock@gmail.com>
2015-08-19 17:57:33 +0800

    queue server/client was fully UP.

--- System/queue/client.js
+++ System/queue/client.js
@@ -1,24 +1,8 @@
-var net = require("net");
-var port = 12345;
-var client = new net.Socket();
-var sockPath = "/tmp/fdsock/light_queue.sock";
+var client = require("./clientUtils");
 
-var connectionListener = function () {
-    console.log("I'm connected.");
-
-    var json = {};
-    json.action = "SET";
-    json.name = "holy";
-    json.data = {"sex": "female", "age": 20};
-
-    client.write(JSON.stringify(json));
-};
-
-client.connect(sockPath, connectionListener);
-client.on('data', function (data) {
-    console.log('Server Response: ', data.toString());
-    client.end();
-});
-client.on('end', function () {
-    console.log("I'm disconnected");
+client.Get("holy", function(data) {
+    console.log("RESP: ", data);
+    client.Get("holy", function(data) {
+        console.log("RESP: ", data);
+    });
 });
