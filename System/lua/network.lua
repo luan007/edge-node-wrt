870309a35cc9edd533273799a4a276ab9e46ae2e
rockdragon <moyerock@gmail.com>
2015-09-07 14:25:09 +0800

    wan scheme .

--- System/lua/network.lua
+++ System/lua/network.lua
@@ -4,6 +4,9 @@ local inspect = require "inspect"
 local process = require "process"
 local cjson = require "cjson"
 
+local ppp_secrets = "/etc/ppp/pap-secrets"
+local chap_secrets = "/etc/ppp/chap-secrets"
+
 local cmd = "dnsmasq"
 local dnsmasq_conf = "/etc/dnsmasq.conf"
 local status_conf = "/etc/dnsmasq.status.json"
@@ -29,7 +32,7 @@ end
 
 function mini_conf(network_address, dhcp_start, dhcp_end, domain)
     return
-            "dhcp-option=6,"..network_address.."\n"..
+    "dhcp-option=6,"..network_address.."\n"..
             "dhcp-range="..dhcp_start..","..dhcp_end.."\n"..
             "listen-address="..network_address..",127.0.0.1\n"..
             "expand-hosts\n"..
@@ -48,15 +51,35 @@ function mini_conf(network_address, dhcp_start, dhcp_end, domain)
             "servers-file=/etc/dnsmasq.server.file.conf\n"
 end
 
+function start_pppd(account, pwd, number)
+    local secrets = '"'..account..'"\t*\t"'..pwd..'"'
+    write_file(ppp_secrets, secrets)
+    write_file(chap_secrets, secrets)
+    local pppd = process.new()
+    pppd.start("pppd", "plugin", "/usr/lib/pppd/2.4.7/rp-pppoe.so", "name", account, "unit", number, "noipdefault", "defaultroute", "mtu", "1492", "mru", "1492", "lock", "ipv6", "usepeerdns", "nodetach")
+    return pppd
+end
+
+function start_udhcpc(interface)
+    local udhcpc = process.new()
+    udhcpc.start("udhcpc", "-i", interface, "-f")
+    return udhcpc
+end
+
 bootstrap(function()
     local daemon = process.new()
 
     onStatChange(status_conf, function(p, t)
-        local status = cjson.decode(read_file(status_conf))
-        local content = mini_conf(status.network_address
-            , status.dhcp_start
-            , status.dhcp_end
-            , status.domain)
+        local config = cjson.decode(read_file(status_conf))
+        if config.wan and config.wan.scheme and config.wan.scheme == "udhcpc" then
+            start_udhcpc("eth2")
+        else
+            start_pppd(config.wan.ppp.account, config.wan.ppp.passwd, config.wan.ppp.number)
+        end
+        local content = mini_conf(config.network_address
+            , config.dhcp_start
+            , config.dhcp_end
+            , config.domain)
         print(content)
         write_file(dnsmasq_conf, content)
         daemon:start(cmd, "-C", dnsmasq_conf, "-k")
